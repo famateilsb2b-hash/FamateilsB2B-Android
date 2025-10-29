bf54d06b3b4d76f2117cd29a891f7a8a5cdb9b26
famateilsb2b-hash <famateilsb2b@gmail.com>
2025-10-29 19:45:37 +0100

    Add files via upload

---
+++ FamateilsB2B-Android-Full/README.md
@@ -0,0 +1,2 @@
+# FamateilsB2B Android App
+Simple Android WebView app for https://www.famateilsb2b.com
---
+++ FamateilsB2B-Android-Full/app/src/main/java/com/famateilsb2b/android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.famateilsb2b.android;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.famateilsb2b.com");
+        setContentView(webView);
+    }
+}
